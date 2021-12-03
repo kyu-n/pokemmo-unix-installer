d67d198fd6580bd380e235d3fcbef31af675d945
Kyu <kyu@pokemmo.eu>
2021-12-02 23:06:55 -0600

    Remove dead mirror from rotation

--- src/main/java/com/pokeemu/unix/updater/MainFeed.java
+++ src/main/java/com/pokeemu/unix/updater/MainFeed.java
@@ -28,7 +28,6 @@ public class MainFeed
 	public static final String[] DOWNLOAD_MIRRORS = {
 			"https://dl.pokemmo.eu/",
 			"https://files.pokemmo.eu/",
-			"https://dl.pokemmo.com/",
 			"https://dl.pokemmo.download/"
 	};
 
@@ -41,36 +40,38 @@ public class MainFeed
 	{
 		String sig_format = "SHA256withRSA";
 		PublicKey pub_key = CryptoUtil.getFeedsPublicKey();
-		
-		for(int feed_id = 0; feed_id < DOWNLOAD_MIRRORS.length; feed_id++)
+
+		for(String mirror : DOWNLOAD_MIRRORS)
 		{
 			try
 			{
-				byte[] raw = Util.getBytes(new URL(DOWNLOAD_MIRRORS[feed_id]+"/feeds/main_feed.txt").openStream());
-				byte[] signature = Util.getBytes(new URL(DOWNLOAD_MIRRORS[feed_id]+"/feeds/main_feed.sig256").openStream());
-				
+				byte[] raw = Util.getBytes(new URL(mirror+"/feeds/main_feed.txt").openStream());
+				byte[] signature = Util.getBytes(new URL(mirror+"/feeds/main_feed.sig256").openStream());
+
 				if(!CryptoUtil.verifySignature(raw, signature, pub_key, sig_format))
 				{
-					mainFrame.showInfo("status.networking.feed_load_failed_validation", feed_id, "INVALID_001");
+					mainFrame.showInfo("status.networking.feed_load_failed_validation", mirror, "INVALID_001");
 					throw new RuntimeException("Error verifying feed signature.");
 				}
-				
+
 				DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-				
+
 				DocumentBuilder db = dbf.newDocumentBuilder();
 				InputSource is = new InputSource(new StringReader(new String(raw)));
 				Document doc = db.parse(is);
-				
+
 				Element main_feed = (Element)doc.getElementsByTagName("main_feed").item(0);
-				
+
 				if(main_feed.getElementsByTagName("min_revision").getLength() > 0)
 					MIN_REVISION = Integer.parseInt(main_feed.getElementsByTagName("min_revision").item(0).getTextContent());
+
+				break;
 			}
 			catch (Exception e) // Something really bad happened. (XML formatting issue / fatal networking error / network signature feed failure)
 			{
 				e.printStackTrace();
 
-				mainFrame.showInfo(Config.getString("status.networking.feed_load_failed_alt", feed_id));
+				mainFrame.showInfo(Config.getString("status.networking.feed_load_failed_alt", mirror));
 				mainFrame.showError(Config.getString("status.networking.feed_load_failed"), Config.getString("status.title.fatal_error"),  () -> System.exit(UnixInstaller.EXIT_CODE_NETWORK_FAILURE));
 
 				return;
